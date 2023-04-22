34c02cffd001f193c4f62bca008370fb157e1842
Mike <mike.zubovich@gmail.com>
2023-04-22 19:33:43 +0300

    added display of pizzas on the page by sorting

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Categories({value, onClickCategory}) {
+function Categories({value, onChangeCategory}) {
 const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 
 	return (
 		<div className="categories">
 		<ul>
 		{categories.map((categoryName, i) => (
-			<li key={i} onClick={() => onClickCategory(i)} className={value === i ? 'active' : ''}>{categoryName}</li>
+			<li key={i} onClick={() => onChangeCategory(i)} className={value === i ? 'active' : ''}>{categoryName}</li>
 		))}
 		</ul>
 	</div>
